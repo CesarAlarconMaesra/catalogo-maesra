914046d3ba86d801641e2dd893c313d365e658ad
CesarAlarconMaesra <cesar.alarcon@maesra.com>
2026-02-09 16:04:48 -0700

    Chnage 1.23

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -30,7 +30,13 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
   );
 });
