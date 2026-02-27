be1ceb6214a62f69d61cdcb0d630531b0e0445c8
CesarAlarconMaesra <cesar.alarcon@maesra.com>
2026-02-27 10:26:31 -0700

    Add files via upload

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,22 +1,24 @@
-const CACHE_NAME = "maesra-cache-v16";
+const CACHE_NAME = "maesra-cache-v17";
 
 const urlsToCache = [
   "/catalogo-maesra/",
   "/catalogo-maesra/index.html",
   "/catalogo-maesra/style.css",
   "/catalogo-maesra/app.js",
-  "/catalogo-maesra/productos.json",
   "/catalogo-maesra/manifest.json",
   "/catalogo-maesra/img/sin_imagen.jpg"
 ];
 
+// INSTALACIÓN
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// ACTIVACIÓN
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys => {
@@ -24,31 +26,36 @@ self.addEventListener("activate", event => {
         keys.filter(key => key !== CACHE_NAME)
             .map(key => caches.delete(key))
       );
-    })
+    }).then(() => self.clients.claim())
   );
 });
 
+// FETCH
 self.addEventListener("fetch", event => {
+
+  // NO cachear productos.json (para que precios se actualicen)
+  if (event.request.url.includes("productos.json")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        if (response) {
-          return response;
-        }
+        if (response) return response;
 
         return fetch(event.request)
           .then(fetchResponse => {
-            return caches.open(CACHE_NAME).then(cache => {
-              if(request.method === "GET"){
-  		cache.put(request, response.clone());
-		}
-              return fetchResponse;
-            });
+            if (event.request.method === "GET") {
+              const responseClone = fetchResponse.clone();
+              caches.open(CACHE_NAME)
+                .then(cache => cache.put(event.request, responseClone));
+            }
+            return fetchResponse;
           })
           .catch(() => {
-            // Si es imagen y falla, mostrar imagen por defecto
             if (event.request.destination === "image") {
-              return caches.match("./img/sin_imagen.jpg");
+              return caches.match("/catalogo-maesra/img/sin_imagen.jpg");
             }
           });
       })
